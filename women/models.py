408548b412dbc78c3f252231c97d28fac5cbce9c
Axsel <Axsel1111@bk.ru>
2024-05-31 11:45:59 +0300

    BD_COM3

--- women/models.py
+++ women/models.py
@@ -1,9 +1,35 @@
 from django.db import models
+from django.urls import reverse
+
+
+class PudlMan(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_publishet=Women.Status.PUBLISHED)
 
 
 class Women(models.Model):
+    class Status(models.IntegerChoices):
+        DRAFT = 0, 'Черновик'
+        PUBLISHED = 1, 'Опубликованно'
+
     title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
+    is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT)
+
+    objects = models.Manager()
+    publishet = PudlMan()
+
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'post_slug': self.slug})
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=250, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
--- women/views.py
+++ women/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from women.models import Women
 
 menu = [
     {'title': 'o сайте', 'url_name': 'about'},
@@ -9,16 +11,16 @@ menu = [
 ]
 
 
-
 data_db = [{'id': 1, 'title': 'Второе значение', 'cobyent': 'третье значение'},
            {'id': 2, 'title': '2 Второе значение', 'cobyent': '2 третье значение'},
            {'id': 3, 'title': '3 Второе значение', 'cobyent': '3 третье значение'}]
 
 
 def index(request):
+    posts = Women.objects.filter(is_published=1)
     data = {
         'titel': 'главная страница',
-        'posts': data_db,
+        'posts': posts,
         'menu': menu
     }
     return render(request, "women/index.html", context=data, )
@@ -28,8 +30,16 @@ def about(request):
     return render(request, 'women/about.html', {'titel': 'О сайте', 'menu': menu})
 
 
-def show_post(request, post_id):
-    return HttpResponse(f"Отображение статьи с id = {post_id}")
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
+
+    data = {
+        'title': post.title,
+        'menu': menu,
+        'post': post,
+        'cat_selected': 1,
+    }
+    return render(request, 'women/post.html', data)
 
 
 def addpage(request):
@@ -42,3 +52,14 @@ def contact(request):
 
 def login(request):
     return HttpResponse('авторизация')
+
+
+def show_category(request, cat_id, date_db=None):
+    data = {
+        'title': 'отображение по рубрикам',
+        'menu': menu,
+        'posts': date_db,
+        'cat_selected': cat_id,
+    }
+
+    
